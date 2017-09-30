1592f9d14d11e612f05768bcf6df04cddbfb0928
preineck <preineck2@gmail.com>
2017-09-30 07:35:50 -0500

    node tutorial

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log('starting app.js');
+
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes.js');
+
+const argv = yargs.argv;
+var command = process.argv[2];
+console.log('Command', command);
+console.log('Process: ',process.argv);
+console.log('Yargs', argv);
+
+
+if (command === 'add') {
+    notes.addNote(argv.title, argv.body);
+}   else if (command === 'list') {
+
+}   else if (command === 'read') {
+
+}   else if (command === 'remove') {
+
+}   else {
+    console.log('unknown command');
+}
---
+++ notes.js
@@ -0,0 +1,11 @@
+console.log('starting notes.js')
+
+var addNote = (title, body) => {
+   console.log('Adding note', title, body) ;
+}
+
+module.exports = {
+    addNote     //addNote = addNote is...old syntax when object /function are same
+}
+
+// ES6 syntax
